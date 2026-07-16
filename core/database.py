6dabf5e41426893890ab1177a1a57e6d8daf75cc
Viraj Rajaka <139100206+rajaka43@users.noreply.github.com>
2026-07-16 13:59:23 +0530

    Remove singleton database instance and getter function
    Removed singleton pattern for database instance.

--- core/database.py
+++ core/database.py
@@ -78,14 +78,3 @@ class MockDatabase:
             
         return df
 
-# Singleton Database instance wrapper
-_db_instance = None
-
-def get_database() -> MockDatabase:
-    """
-    Returns the centralized database instance
-    """
-    global _db_instance
-    if _db_instance is None:
-        _db_instance = MockDatabase()
-    return _db_instance
